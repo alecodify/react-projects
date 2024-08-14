856f5388006d5508838c9176317f7a805efaa086
aliraza <chaudaryaliraza0@gmail.com>
2024-08-14 12:00:05 +0500

    #6 completed

---
+++ 06-color-generator/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import Values from 'values.js';
+import Color from './Color';
+
+function App() {
+  const [color, setColor] = useState('')
+  const [error, setError] = useState(false)
+  const [list, setList] = useState(new Values('#f15025').all(10))
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+
+    try {
+      let colors = new Values(color).all(10)
+      setList(colors)
+    } catch (error) {
+      setError(true)
+    }
+  }
+
+  return (
+    <div>
+      <section className='container'>
+          <h3>color generator</h3>
+          <form onSubmit={handleSubmit}>
+            <input type="text" value={color} onChange={(e) => setColor(e.target.value)} placeholder='#f15025' className={`${error ? 'error': ''}`} />
+            <button className='btn' type='submit'>submit</button>
+          </form>
+      </section>
+
+      <section className='colors'>
+        {list.map((color, index) => (
+          <Color key={index} {...color} hexColor={color.hex} index={index} />
+        ))}
+      </section>
+    </div>
+  )
+}
+
+export default App
